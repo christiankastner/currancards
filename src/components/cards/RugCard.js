940e10c95f816542c734163ed30da24bbae1369b
Christian Kastner <christianmkastner@gmail.com>
2020-07-24 10:55:42 -0700

    Fixed some accessiblity contrast and alt tag issues, as well as swatch buttons

--- src/components/cards/RugCard.js
+++ src/components/cards/RugCard.js
@@ -2,22 +2,25 @@ import React, {useState} from "react";
 import Style from "./RugCard.module.scss";
 import {classNames} from "../../services/utils"
 
+const maxColors = 5;
+
 export default function RugCard({rug}) {
     const [selected, setSelected] = useState(0)
-    const maxColors = 8;
     const renderSwatches = () => {
         if (!rug.hasOwnProperty("colors")) return
         return rug.colors.slice(0,maxColors).map((color,index) => {
             return (
-                    <button className={classNames(index === selected ? Style.swatchActive : "",Style.swatch)} title={color.color} style={{background: `url(${color.image})`}} onClick={() => setSelected(index)}>
-                    </button>
+                <div className={classNames(index === selected ? Style.swatchActive : "", Style.swatchContainer)} >
+                    <p className={Style.swatch} title={color.color} style={{background: `url(${color.image})`}} onClick={() => setSelected(index)}>
+                    </p>
+                </div>
             )
     })}
 
     return (
         <div className={Style.rugCard}>
             <a href={rug.link}>
-                <img src={rug.colors[selected].image}/>
+                <img alt={`${rug.name} ${rug.colors[selected].color}`} src={rug.colors[selected].image}/>
             </a>
             <div className={Style.content}>
                 <a href={rug.link}>
@@ -26,7 +29,7 @@ export default function RugCard({rug}) {
                 </a>
             <div className={Style.footer}>
                 {renderSwatches()}
-                {rug.colors.length > maxColors && <a href="#">+{rug.colors.length - maxColors}</a>}
+                {rug.colors.length > maxColors && <a href={rug.link}>+{rug.colors.length - maxColors}</a>}
             </div>
             </div>
         </div>
